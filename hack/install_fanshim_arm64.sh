8401a5604f6e15a1c5cc8011e25e7bbb60386fbb
Johan Siebens <johan.siebens@gmail.com>
2021-05-18 11:52:49 +0200

    Update install_fanshim_arm64.sh

--- hack/install_fanshim_arm64.sh
+++ hack/install_fanshim_arm64.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo systemctl stop fanshim || true
 
-sudo curl -sL -o /usr/local/bin/fanshim_linux_arm64 https://github.com/jsiebens/fanshim-go/releases/download/v0.6.0/fanshim_linux_arm64
+sudo curl -sL -o /usr/local/bin/fanshim_linux_arm64 https://github.com/jsiebens/fanshim-go/releases/download/v0.7.0/fanshim_linux_arm64
 sudo chmod 755 /usr/local/bin/fanshim_linux_arm64
 
 sudo mkdir -p /etc/fanshim.d
